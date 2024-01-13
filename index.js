aa460a8cfe9fba5684d1dc81073c0dd0be4fcfa7
Abdul Basit <abdlbasit003@gmail.com>
2024-01-13 22:12:31 +0500

    Delete bug fixed

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ let taskPrio = document.getElementById("newTaskPrio");
 let notification = document.getElementById("notification");
 let notificationTitle = document.getElementById("notification-title");
 
-
 window.onload = () => {
   if (!localStorage.getItem("tasks")) {
     let tasksList = [];
@@ -102,9 +101,6 @@ function createEditIcon(divToAppend) {
   iconSvg.setAttribute("stroke", "black");
   iconSvg.setAttribute("stroke-width", "1.5");
   iconSvg.classList.add("icon");
-  iconSvg.onclick = function (e) {
-    editTask(e);
-  };
 
   iconPath.setAttribute(
     "d",
@@ -130,9 +126,6 @@ function createDeleteIcon(divToAppend) {
   iconSvg.setAttribute("stroke", "black");
   iconSvg.setAttribute("stroke-width", "1.5");
   iconSvg.classList.add("icon");
-  iconSvg.onclick = function (e) {
-    deleteTask(e);
-  };
   iconPath.setAttribute(
     "d",
     "m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
@@ -211,9 +204,21 @@ function addTaskToBoard(task) {
   taskPrioSpan.className = "task-prio";
   taskDueDateSpan.className = "task-due-date";
   let taskActionsDiv = document.createElement("div");
-  taskActionsDiv.clasName = "task-actions";
-  createEditIcon(taskActionsDiv);
-  createDeleteIcon(taskActionsDiv);
+  taskActionsDiv.className = "task-actions";
+  let editDiv = document.createElement("div");
+  let deleteDiv = document.createElement("div");
+  editDiv.className = "edit-icon";
+  editDiv.onclick = function (e) {
+    editTask(e);
+  };
+  deleteDiv.className = "del-icon";
+  deleteDiv.onclick = function (e) {
+    deleteTask(e);
+  };
+  createEditIcon(editDiv);
+  createDeleteIcon(deleteDiv);
+  taskActionsDiv.append(editDiv);
+  taskActionsDiv.append(deleteDiv);
   if (priority === "Low") {
     taskPrioSpan.classList.add("green");
   } else if (priority === "Medium") {
@@ -270,7 +275,8 @@ function createAddIcon(divToAppend) {
 }
 
 function editTask(e) {
-  let taskDiv = e.target.parentElement.parentElement.parentElement;
+  let taskDiv =
+    e.target.parentElement.parentElement.parentElement.parentElement;
   let taskUuid = taskDiv.getAttribute("data-uuid");
   if (taskUuid != null || taskUuid != undefined) {
     let task = getFromLocalStorage(taskUuid);
@@ -306,18 +312,15 @@ function updateTask(uuid, board) {
 }
 
 function deleteTask(e) {
-  let taskDiv = e.target.parentElement.parentElement.parentElement;
-  taskDiv.classList.remove("show-animation");
+  let taskDiv =
+    e.target.parentElement.parentElement.parentElement.parentElement;
   let uuid = taskDiv.getAttribute("data-uuid");
   let tasksList = JSON.parse(localStorage.getItem("tasks"));
+  tasksList = tasksList.filter((task) => task.uuid !== uuid);
 
-  for (task of tasksList) {
-    if (task.uuid === uuid) {
-      tasksList.pop(task);
-      break;
-    }
-  }
   localStorage.setItem("tasks", JSON.stringify(tasksList));
+
+  taskDiv.classList.remove("show-animation");
 
   setTimeout(() => {
     window.location.reload();
